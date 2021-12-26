f6c892242b7cf51d72b7f2d205c558a8ca11a425
m0611104 <m0611104@gcloud.csu.edu.tw>
2021-12-26 10:08:35 +0800

    implement laptop camera with picture in picture

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ const button = document.getElementById('button');
 // Prompt user to select media stream, pass to video element, then play
 async function selectMediaStream() {
 	try {
-		const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+		//const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+
+		//get laptop camera, with picture in picture
+		var constraints = { audio: false, video: { width: 1280, height: 720 } };
+		const mediaStream = navigator.mediaDevices.getUserMedia(constraints)
+		.then(function(mediaStream) {
+		  var video = document.querySelector('video');
+			  video.srcObject = mediaStream;
+			  video.onloadedmetadata = function(e) {
+			    video.play();
+			  };
+			})
+		.catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
 		//the media stream will pass to a video source object 
 		videoElement.srcObject = mediaStream;
 		//once video is loaded, the video will play
